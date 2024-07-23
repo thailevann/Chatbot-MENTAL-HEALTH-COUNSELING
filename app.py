9d8e0d729179f58759939119ebb64905aa0feb4e
Thái Lê Vân <82956224+thailevann@users.noreply.github.com>
2024-07-23 19:46:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -178,11 +178,6 @@ def simulate_conversation():
           st.session_state.messages = []
 
       response = retrival_reraking("./data", query2, index)
-      nf4_config = BitsAndBytesConfig(load_in_4bit=True,bnb_4bit_quant_type="nf4",bnb_4bit_use_double_quant=True,bnb_4bit_compute_dtype=torch.bfloat16)
-      model_id = "Viet-Mistral/Vistral-7B-Chat"
-      hf_token = "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"
-      model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=nf4_config, token=hf_token)
-      hf_llm = HuggingFaceLLM(model=model, tokenizer=tokenizer)
       
       with st.chat_message("assistant"):
               st.markdown(response)
